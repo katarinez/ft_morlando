4c5066df7853e2990583bbbb86812b8f01e8dae0
bmstu <BaumanUniversity@MacBook-Pro-Schurenkovs.local>
2019-09-23 14:59:34 +0300

    second

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -1,50 +1,28 @@
 #include <stdlib.h>
 
-int		ft_lenof(char const *s)
-{
-	int i;
-	int j;
-	int k;
-
-	i = 0;
-	k = 0;
-	while (*(s + k))
-	{
-		if ((*(s + k) == ' ' || *(s + k) == '\t' || *(s + k) == '\n' || *(s + k) == ',') && (j == 1))
-			i++;
-		else if (*(s + k) == ' ' || *(s + k) == '\t' || *(s + k) == '\n' || *(s + k) == ',')
-			j = 0;
-		else 
-		{
-			i++;
-			j = 1;
-		}
-		k++;
-	}
-	return (i);
-}
-
 char	*ft_strtrim(char const *s)
 {
 	int	i;
-	int	j;
-	int	k;
+	int	len;
 	char	*str;
 
-	i = ft_lenof(s);
-	str = malloc(sizeof(*str) * (i + 1));
-	i = 0;
-	k = 0;
-	j = i;
-	while (*(s + i) == ' ' || *(s + i) == '\t' || *(s + i) == '\n' || *(s + i) == ',')
-		i++;
-	while (j)
-	{
-		*(str + k) = *(s + i);
-		i++;
-		k++;
-		j--;
-	}
-	*(str + k) = '\0';
+	if (!s)
+		return (0);
+	len = ft_strlen(s);
+	while (s[len - 1] == ' ' || s[len - 1] == '\t' || s[len - 1] == '\n')
+		len--;
+	i = -1;
+	while (s[++i] == ' ' || s[i] == '\t' || s[i] == '\n')
+		len--;
+	if (len <= 0)
+		len = 0;
+	str = (char*)malloc(sizeof(char) * (len + 1));
+	if (!str)
+		return (0);
+	s += i;
+	i = -1;
+	while (++i < len)
+		str[i] = *s++;
+	str[i] = '\0';
 	return (str);
 }
